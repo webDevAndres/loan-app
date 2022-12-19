def0abdf32db6c8cc540e1156eef9574703bbf34
webDevAndres <macias.andres02@gmail.com>
2022-12-19 01:30:09 -0600

    fixed bug with calculation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -36,7 +36,7 @@ export class HomeComponent implements OnInit {
   this.monthlyPayment = (loanAmount *(monthlyInterest * Math.pow(monthlyInterest + 1, numOfMonths))) /
     (Math.pow(1 + monthlyInterest, numOfMonths) - 1);
 
-    this.monthlyPayment = parseFloat(this.monthlyPayment.toFixed(2));
+  this.monthlyPayment = parseFloat(this.monthlyPayment.toFixed(2));
 
   this.interest = this.monthlyPayment * numOfMonths - loanAmount;
 }
